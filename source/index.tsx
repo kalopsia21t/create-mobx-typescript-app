bdaf1822b61c68f3747f7e5219cd0d9126f506ce
tpaliy <taras.paliy@bytebrand.net>
2018-08-20 17:27:14 +0300

    tsconfig

---
+++ source/index.tsx
@@ -0,0 +1,15 @@
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+
+// class App extends React.Component<{}, {}> {
+//     render(): JSX.Element {
+//         return (<div>
+//             REACT YEAH
+//         </div>);
+//     }
+// }
+
+// console.log(12313);
+// ReactDOM.render(<App />, document.getElementById('#root'));
+
+console.log(12414);
